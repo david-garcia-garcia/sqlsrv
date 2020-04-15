9db7bfd23118dd38ea82466c23c6d00581744015
Beakerboy <beakerboy@1700098.no-reply.drupal.org>
2020-04-15 15:29:47 +0000

    Issue #2782793 by Antonnavi, jantoine: Make use of wincache for query string optional, Issue #2853000 by maijs: [FR] Do not require wincache

--- drivers/lib/Drupal/Driver/Database/sqlsrv/Connection.php
+++ drivers/lib/Drupal/Driver/Database/sqlsrv/Connection.php
@@ -640,7 +640,13 @@ class Connection extends DatabaseConnection {
 
     // Drill through everything...
     $success = FALSE;
-    $cache = wincache_ucache_get($query_signature, $success);
+    $cache = '';
+    if (extension_loaded('wincache')) {
+      $cache = wincache_ucache_get($query_signature, $success);
+    }
+    elseif (extension_loaded('apcu') && (PHP_SAPI !== 'cli' || (bool) ini_get('apc.enable_cli'))) {
+      $cache = apcu_fetch($query_signature, $success);
+    }
     if ($success) {
       return $cache;
     }
@@ -677,7 +683,12 @@ class Connection extends DatabaseConnection {
 
     // Store the processed query, and make sure we expire it some time
     // so that scarcely used queries don't stay in the cache forever.
-    wincache_ucache_set($query_signature, $query, rand(600, 3600));
+   if (extension_loaded('wincache')) {
+      wincache_ucache_set($query_signature, $query, rand(600, 3600));
+    }
+    elseif (extension_loaded('apcu') && (PHP_SAPI !== 'cli' || (bool) ini_get('apc.enable_cli'))) {
+      apcu_store($query_signature, $query, rand(600, 3600));
+    }
 
     return $query;
   }
